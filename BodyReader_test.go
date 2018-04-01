adc391b953e8f3b61244b0b039a26bc83850e4fa
Eduard Urbach <e.urbach@gmail.com>
2018-04-02 00:16:26 +0200

    Added body reader tests

---
+++ BodyReader_test.go
@@ -0,0 +1,54 @@
+package aero_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/aerogo/aero"
+	"github.com/stretchr/testify/assert"
+	"github.com/tdewolff/buffer"
+)
+
+func TestBodyReader(t *testing.T) {
+	app := aero.New()
+
+	// Register route
+	app.Get("/", func(ctx *aero.Context) string {
+		body := ctx.Request().Body()
+		bodyText, _ := body.String()
+		return ctx.Text(bodyText)
+	})
+
+	// Get response
+	requestBody := []byte(helloWorld)
+	request, _ := http.NewRequest("GET", "/", buffer.NewReader(requestBody))
+	response := httptest.NewRecorder()
+	app.Handler().ServeHTTP(response, request)
+
+	// Verify response
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, helloWorld, response.Body.String())
+}
+
+func TestBodyReaderJSON(t *testing.T) {
+	app := aero.New()
+
+	// Register route
+	app.Get("/", func(ctx *aero.Context) string {
+		body := ctx.Request().Body()
+		obj, _ := body.JSONObject()
+		return ctx.Text(fmt.Sprint(obj["key"]))
+	})
+
+	// Get response
+	requestBody := []byte(`{"key":"value"}`)
+	request, _ := http.NewRequest("GET", "/", buffer.NewReader(requestBody))
+	response := httptest.NewRecorder()
+	app.Handler().ServeHTTP(response, request)
+
+	// Verify response
+	assert.Equal(t, http.StatusOK, response.Code)
+	assert.Equal(t, "value", response.Body.String())
+}
