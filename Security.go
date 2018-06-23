4b7b6aedec3095835ea222106daf62a1d7eb0103
Eduard Urbach <e.urbach@gmail.com>
2018-06-23 11:22:28 +0900

    Added one more cipher suite

--- Security.go
+++ Security.go
@@ -28,11 +28,11 @@ func createTLSConfig() *tls.Config {
 			// ECDSA is about 3 times faster than RSA and should be preferred.
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
 
 			// RSA is slower but still widely used.
 			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
 		},
 	}
 }
