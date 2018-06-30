b1fa640b751edc9363399003d21ca2f4ffd84a6a
Eduard Urbach <e.urbach@gmail.com>
2018-06-30 13:34:10 +0900

    Added a few comments to rewriteHandler

--- RewriteHandler.go
+++ RewriteHandler.go
@@ -2,11 +2,13 @@ package aero
 
 import "net/http"
 
+// rewriteHandler is the replacement handler we use in case we have a Rewrite function.
 type rewriteHandler struct {
 	rewrite func(*RewriteContext)
 	router  http.Handler
 }
 
+// ServeHTTP deals with the request in case we have a Rewrite function.
 func (r *rewriteHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	r.rewrite(&RewriteContext{
 		Request: request,
