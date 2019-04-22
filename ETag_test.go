2cf118d280b4c6674d427ef16175e8d905334d7d
Eduard Urbach <e.urbach@gmail.com>
2019-04-22 14:04:26 +0900

    Added ETagString test

--- ETag_test.go
+++ ETag_test.go
@@ -2,6 +2,7 @@ package aero_test
 
 import (
 	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,6 +16,18 @@ func TestETag(t *testing.T) {
 
 	etag1 := aero.ETag(text1)
 	etag2 := aero.ETag(text2)
+
+	assert.NotEmpty(t, etag1)
+	assert.NotEmpty(t, etag2)
+	assert.NotEqual(t, etag1, etag2)
+}
+
+func TestETagString(t *testing.T) {
+	text1 := strings.Repeat("Hello World", 1000000)
+	text2 := strings.Repeat("Hello Aero", 1000000)
+
+	etag1 := aero.ETagString(text1)
+	etag2 := aero.ETagString(text2)
 
 	assert.NotEmpty(t, etag1)
 	assert.NotEmpty(t, etag2)
