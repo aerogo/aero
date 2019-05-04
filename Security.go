9679053c22990436bdf133b0772d585cdd21a3e8
Eduard Urbach <e.urbach@gmail.com>
2019-05-04 16:35:28 +0900

    Added TLS 1.3 cipher suites

--- Security.go
+++ Security.go
@@ -25,6 +25,11 @@ func createTLSConfig() *tls.Config {
 			tls.CurveP256,
 		},
 		CipherSuites: []uint16{
+			// TLS 1.3
+			tls.TLS_AES_256_GCM_SHA384,
+			tls.TLS_AES_128_GCM_SHA256,
+			tls.TLS_CHACHA20_POLY1305_SHA256,
+
 			// ECDSA is about 3 times faster than RSA on the server side.
 			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
