2a02c345a242890508b9682993ca3ed0a963fa97
Eduard Urbach <e.urbach@gmail.com>
2019-05-21 15:02:59 +0900

    Improved source documentation

--- Listener.go
+++ Listener.go
@@ -14,12 +14,15 @@ type Listener struct {
 
 // Accept accepts incoming connections.
 func (listener Listener) Accept() (net.Conn, error) {
+	// Accept a new client.
 	connection, err := listener.AcceptTCP()
 
 	if err != nil {
 		return nil, err
 	}
 
+	// Set keep-alive values based on the default values
+	// found in the net/http TCP listener implementation.
 	err = connection.SetKeepAlive(true)
 
 	if err != nil {
@@ -32,6 +35,12 @@ func (listener Listener) Accept() (net.Conn, error) {
 		return nil, err
 	}
 
+	// Disable Nagle's algorithm to get lower latency.
+	// NoDelay causes TCP packets to be sent immediately
+	// when we call Write() instead of waiting to possibly
+	// reduce the amount of packets. As Aero is fully aware
+	// of the entire response data for most responses and only
+	// causes a single Write() call, the delay should be disabled.
 	err = connection.SetNoDelay(true)
 
 	if err != nil {
