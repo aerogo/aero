218edb603733a4bbccb319c656498935ba2d9b74
Eduard Urbach <e.urbach@gmail.com>
2019-05-21 15:11:14 +0900

    Split headers to a separate file

--- Context.go
+++ Context.go
@@ -29,37 +29,6 @@ import (
 // we're trying to optimize for performance, not bandwidth.
 const gzipThreshold = 1450
 
-const (
-	cacheControlHeader            = "Cache-Control"
-	cacheControlAlwaysValidate    = "must-revalidate"
-	cacheControlMedia             = "public, max-age=13824000"
-	contentTypeOptionsHeader      = "X-Content-Type-Options"
-	contentTypeOptions            = "nosniff"
-	xssProtectionHeader           = "X-XSS-Protection"
-	xssProtection                 = "1; mode=block"
-	etagHeader                    = "ETag"
-	contentTypeHeader             = "Content-Type"
-	contentTypeHTML               = "text/html; charset=utf-8"
-	contentTypeCSS                = "text/css; charset=utf-8"
-	contentTypeJavaScript         = "application/javascript; charset=utf-8"
-	contentTypeJSON               = "application/json; charset=utf-8"
-	contentTypeJSONLD             = "application/ld+json; charset=utf-8"
-	contentTypePlainText          = "text/plain; charset=utf-8"
-	contentTypeEventStream        = "text/event-stream; charset=utf-8"
-	contentEncodingHeader         = "Content-Encoding"
-	contentEncodingGzip           = "gzip"
-	acceptEncodingHeader          = "Accept-Encoding"
-	contentLengthHeader           = "Content-Length"
-	ifNoneMatchHeader             = "If-None-Match"
-	referrerPolicyHeader          = "Referrer-Policy"
-	referrerPolicySameOrigin      = "no-referrer"
-	strictTransportSecurityHeader = "Strict-Transport-Security"
-	strictTransportSecurity       = "max-age=31536000; includeSubDomains; preload"
-	contentSecurityPolicyHeader   = "Content-Security-Policy"
-	forwardedForHeader            = "X-Forwarded-For"
-	realIPHeader                  = "X-Real-Ip"
-)
-
 // Push options describes the headers that are sent
 // to our server to retrieve the push response.
 var pushOptions = http.PushOptions{
---
+++ Headers.go
@@ -0,0 +1,34 @@
+package aero
+
+// This list includes all the common header keys
+// and values used in the http server code.
+const (
+	cacheControlHeader            = "Cache-Control"
+	cacheControlAlwaysValidate    = "must-revalidate"
+	cacheControlMedia             = "public, max-age=13824000"
+	contentTypeOptionsHeader      = "X-Content-Type-Options"
+	contentTypeOptions            = "nosniff"
+	xssProtectionHeader           = "X-XSS-Protection"
+	xssProtection                 = "1; mode=block"
+	etagHeader                    = "ETag"
+	contentTypeHeader             = "Content-Type"
+	contentTypeHTML               = "text/html; charset=utf-8"
+	contentTypeCSS                = "text/css; charset=utf-8"
+	contentTypeJavaScript         = "application/javascript; charset=utf-8"
+	contentTypeJSON               = "application/json; charset=utf-8"
+	contentTypeJSONLD             = "application/ld+json; charset=utf-8"
+	contentTypePlainText          = "text/plain; charset=utf-8"
+	contentTypeEventStream        = "text/event-stream; charset=utf-8"
+	contentEncodingHeader         = "Content-Encoding"
+	contentEncodingGzip           = "gzip"
+	acceptEncodingHeader          = "Accept-Encoding"
+	contentLengthHeader           = "Content-Length"
+	ifNoneMatchHeader             = "If-None-Match"
+	referrerPolicyHeader          = "Referrer-Policy"
+	referrerPolicySameOrigin      = "no-referrer"
+	strictTransportSecurityHeader = "Strict-Transport-Security"
+	strictTransportSecurity       = "max-age=31536000; includeSubDomains; preload"
+	contentSecurityPolicyHeader   = "Content-Security-Policy"
+	forwardedForHeader            = "X-Forwarded-For"
+	realIPHeader                  = "X-Real-Ip"
+)
