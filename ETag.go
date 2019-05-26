ee11578deb186bc37402098283fadeda92f50aef
Eduard Urbach <e.urbach@gmail.com>
2019-05-26 14:47:10 +0900

    Faster hashing for E-Tags (~300%)

--- ETag.go
+++ ETag.go
@@ -3,29 +3,17 @@ package aero
 import (
 	"strconv"
 
-	"github.com/OneOfOne/xxhash"
+	"github.com/akyoto/hash"
 )
 
 // ETag produces a hash for the given slice of bytes.
 // It is the same hash that Aero uses for its ETag header.
 func ETag(b []byte) string {
-	h := xxhash.NewS64(0)
-
-	// This function never fails according to the source code,
-	// so we ignore checking the error value.
-	_, _ = h.Write(b)
-
-	return strconv.FormatUint(h.Sum64(), 16)
+	return strconv.FormatUint(hash.Bytes(b), 16)
 }
 
 // ETagString produces a hash for the given string.
 // It is the same hash that Aero uses for its ETag header.
 func ETagString(b string) string {
-	h := xxhash.NewS64(0)
-
-	// This function never fails according to the source code,
-	// so we ignore checking the error value.
-	_, _ = h.WriteString(b)
-
-	return strconv.FormatUint(h.Sum64(), 16)
+	return strconv.FormatUint(hash.String(b), 16)
 }
--- go.mod
+++ go.mod
@@ -3,17 +3,18 @@ module github.com/aerogo/aero
 go 1.12
 
 require (
-	github.com/OneOfOne/xxhash v1.2.5
 	github.com/aerogo/csp v0.1.6
 	github.com/aerogo/http v1.0.6
 	github.com/aerogo/linter-performance v1.0.3
 	github.com/aerogo/session v0.1.4
 	github.com/aerogo/session-store-memory v0.1.5
 	github.com/akyoto/color v1.8.5
+	github.com/akyoto/hash v0.1.1
 	github.com/akyoto/stringutils v0.2.0
 	github.com/frankban/quicktest v1.4.0
 	github.com/json-iterator/go v1.1.6
 	github.com/julienschmidt/httprouter v1.2.0
 	github.com/tdewolff/parse v2.3.4+incompatible
 	github.com/tdewolff/test v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20190526052359-791d8a0f4d09 // indirect
 )
